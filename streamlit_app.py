d9110f3ac774161ff542f368c67199ad3cf89f95
DavidMaciasJ <130338285+DavidMaciasJ@users.noreply.github.com>
2023-04-16 22:06:03 -0600

    Update streamlit_app.py
    trying to add info but with an error

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,5 @@ streamlit.dataframe(my_data_rows)
 streamlit.header('What would you like to add?')
 add_my_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
 streamlit.write('Thanks for adding: ', add_my_fruit)
+#try to add to a table
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
