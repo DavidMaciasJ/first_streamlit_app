2b68fa2747e0730e048bef702346667d77cd2cbd
DavidMaciasJ <130338285+DavidMaciasJ@users.noreply.github.com>
2023-04-16 22:24:21 -0600

    Update streamlit_app.py
    added handling error code in fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,20 +23,28 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    streamlit.error('Please enter a fruit to get info')
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    #streamlit.text(fruityvice_response.json())
+    # write your own comment -what does the next line do? transforms the json text to data frame
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # write your own comment - what does this do? shows the dataframe
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-#streamlit.text(fruityvice_response.json())
-# write your own comment -what does the next line do? transforms the json text to data frame
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do? shows the dataframe
-streamlit.dataframe(fruityvice_normalized)
-import requests
-import snowflake.connector
+
 streamlit.stop()
 #from fequirements.txt
+
+
+
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
